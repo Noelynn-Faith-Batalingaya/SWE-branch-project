c9bca292dcdcc8dcba304c10e130cbc8937541c7
Theo Jons <theojons@Theos-MacBook-Air-248.local>
2025-02-12 13:21:43 -0500

    half done python classes file

---
+++ classeslab.py
@@ -0,0 +1,12 @@
+class Plant:
+    def __init__(self, genus, species):
+        self.genus = genus
+        self.species = species
+
+    def plantname():
+        print("The genus is: "  genus " and the species is:" species)
+
+class Grass(Plant): 
+    def __init__(self, genus, species):
+        self.genus = genus
+        self.species = species
